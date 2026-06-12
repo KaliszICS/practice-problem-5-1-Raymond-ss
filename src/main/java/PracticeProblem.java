450df626c7565dbe7342ec21d742fd42d654bb56
Raymond-ss <343383188@tdsb.ca>
2026-06-12 16:00:09 +0000

    Finish 5.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,27 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray() {
+		int[] arr;
+		arr = new int[5];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i +1;
+		}
+		return arr;                                                              
+	}
+
+
+	public static String[] createArray(String s1, String s2, String s3, String s4) {
+		String[] word = {s1,s2,s3,s4};
+		return word;
+	}
+
+
+	public static String getElement(int n, String[] arr3){
+		return arr3[n];
+	}
+
+	public static void replaceElement(int n, double value, double[] arr4){
+		System.out.print(arr4[n] = value);
+	}
 }
